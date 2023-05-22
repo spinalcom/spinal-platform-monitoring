159cb5df3dfb7c82548ea672df56b5183522b0cc
aguili <h.aguili@spinalcom.com>
2023-05-22 12:31:44 +0200

    delete node modules

--- node_modules/spinal-env-viewer-graph-service/README.md
+++
@@ -1,5 +0,0 @@
-# spinal-env-viewer-graph-service
-
-Service warper of the spinal-model-graph.
-
-## [Documentation](./docs/globals.md)
--- node_modules/spinal-env-viewer-graph-service/declarations/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { GraphManagerService as GraphManagerServiceClass } from './GraphManagerService';
-export { SpinalNodeRef, InfoModel, SpinalNodeObject } from './GraphManagerService';
-import { SpinalSet, SpinalNodePointer, SpinalMap } from 'spinal-model-graph';
-export { SPINAL_RELATION_TYPE, SPINAL_RELATION_LST_PTR_TYPE, SPINAL_RELATION_PTR_LST_TYPE, SpinalContext, SpinalNode, SpinalGraph, } from 'spinal-model-graph';
-export { SpinalSet, SpinalNodePointer, SpinalMap, };
-declare const SpinalGraphService: GraphManagerServiceClass;
-export { SpinalGraphService, };
